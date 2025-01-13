8a21724400edd66f239d54be5af495e1364132ad
antoine lafitte <antoine.lafitte99411@gmail.com>
2025-01-13 10:57:52 +0100

    2-sort.rb with less modifications from original code

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -7,7 +7,7 @@
 result = []
 ARGV.each do |arg|
     # skip if not integer
-    next unless arg !~ /^-?[0-9]+$/
+    next if arg !~ /^-?[0-9]+$/
 
     # convert to integer
     i_arg = arg.to_i
@@ -16,14 +16,16 @@ ARGV.each do |arg|
     is_inserted = false
     i = 0
     l = result.size
-    result.each_with_index do |val, index|
-        if i_arg < val
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+          i += 1
+        else
             result.insert(i, i_arg)
             is_inserted = true
             break
         end
     end
-    result << i_arg unless !is_inserted
+    result << i_arg if !is_inserted
 end
 
 puts result
